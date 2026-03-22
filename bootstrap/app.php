350e678f12530c700a2950164e5fccc338ac7ce9
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-03-22 14:57:33 +0700

    Fix: Trust all proxies so Vercel HTTPS is recognized and asset URLs use https://

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies so Vercel's HTTPS is recognized correctly
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
